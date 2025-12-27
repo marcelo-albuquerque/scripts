c1407f6911ca9119135b032ffdf187c7a662673e
Marcelo Albuquerque <marrockandroll@gmail.com>
2025-12-27 14:56:58 -0300

    Update noticias.js

--- noticias.js
+++ noticias.js
@@ -1,14 +1,2 @@
-console.log("Script carregado");
-
-const containerShares = document.getElementsByClassName("shade-activitypub");
-for (const container of containerShares) {
-    const elements = container.querySelectorAll("*");
-    elements.forEach(element => {
-        element.innerHTML = element.innerHTML.replace(/admin.guaracinews.com.br/g, "guaracinews.com.br");
-    });
-}
-
-if(window.location.hash === "#/site") {
-    console.log("Redirecting...");
-    window.location.hash = "#/posts";
-}
+console.log("Script carregado!");
+alert("Funciona!");
